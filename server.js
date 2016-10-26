ef14e8492f791a9291deafae2cecd54db0a9aa7c
Trevor <trevdrury@gmail.com>
2016-10-26 19:27:03 +0000

    serves HTML page with description and styling

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express')
-
 var app = express();
+var path = require('path');
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.listen(8080, function () {
